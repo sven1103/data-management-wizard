d032bdf504da7de177b2d679d265c906775a0c9d
sven <sven.fillinger@mailbox.org>
2015-05-17 21:08:51 +0000

    Initial commit

---
+++ src/com/DataManagementWizard.java
@@ -0,0 +1,66 @@
+package com;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.*;
+
+
+@Theme("valo")
+/**
+ * Created by sven on 5/17/15.
+ */
+public class DataManagementWizard extends UI implements Button.ClickListener {
+
+    private boolean flag = true;
+
+    protected Button button;
+    protected Label header2;
+
+    @Override
+    public void init(VaadinRequest request) {
+
+        VerticalLayout content = new VerticalLayout();
+        setContent(content);
+        //content.setSizeFull();
+
+        Label header = new Label("Data Management Tool");
+        header.addStyleName("h1");
+        content.addComponent(header);
+
+        header2 = new Label("Click the button");
+        header2.addStyleName("h2");
+        content.addComponent(header2);
+
+        HorizontalLayout menuview = new HorizontalLayout();
+        menuview.addStyleName("wrapping");
+        //menuview.setSizeFull();
+
+        CssLayout group = new CssLayout();
+        group.addStyleName("v-component-group");
+        menuview.addComponent(group);
+
+        TextField textfield = new TextField();
+        textfield.setValue("Write something nice in here");
+        textfield.setWidth(100.0f, Unit.PERCENTAGE);
+        group.addComponent(textfield);
+        button = new Button("Go");
+        button.addClickListener(this);
+
+        group.addComponent(button);
+        menuview.setSpacing(false);
+        content.addComponent(menuview);
+        content.setExpandRatio(menuview, 1);
+
+    }
+    public void buttonClick(Button.ClickEvent event){
+        if(flag){
+            header2.setValue("Hey, u just did it!");
+            button.setCaption("Click me harder!");
+            flag = false;
+        } else{
+            flag = true;
+            header2.setValue("Click the button!");
+        }
+    }
+
+}
