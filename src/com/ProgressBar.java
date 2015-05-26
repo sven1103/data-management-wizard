0a3afa646e16d1e4bb7c04f4ad97fec931411bf5
Sven Fillinger <sven.fillinger@gmail.com>
2015-05-26 16:09:41 +0200

    progress

---
+++ src/com/ProgressBar.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.util.LinkedList;
+import com.vaadin.ui.*;
+/**
+ * Created by fillinger on 5/26/15.
+ */
+public class ProgressBar {
+    private LinkedList<AUserSlide> userSlideList;
+    private Layout progressBarLayout;
+
+    /**
+     * Constructor for a progress bar element.
+     * @param _userSlideList: expects an LinkedList with user slides
+     */
+    public ProgressBar(LinkedList<AUserSlide> _userSlideList){
+        this.userSlideList = _userSlideList;
+    }
+
+    /**
+     * Builds the progress bar from the user slide elements in the
+     * LinkedList and returns it as a finished layout that can then be
+     * implemented in the parent layout container.
+     * @return: (Layout) finished progress bar layout
+     */
+    public Layout init(){
+         return progressBarLayout;
+    }
+
+    /**
+     * Gets the number of user slides in the linked list
+     * @return: (int) number of user slides
+     */
+    private int getNumberSlides(){
+        return this.userSlideList.size();
+    }
+
+
+}
