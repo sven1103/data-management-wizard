f4c13ed21337c4a05c4d2909f01be64312f2baf0
Sven Fillinger <sven.fillinger@gmail.com>
2015-05-26 17:04:37 +0200

    ProgressBar

--- src/com/ProgressBar.java
+++ src/com/ProgressBar.java
@@ -3,10 +3,17 @@ package com;
 import java.util.LinkedList;
 import com.vaadin.ui.*;
 /**
- * Created by fillinger on 5/26/15.
+ * Implements the progress bar for DMPcreator.
+ * @author Sven Fillinger
  */
 public class ProgressBar {
+    /**
+     * A linked list with containing all user slides
+     */
     private LinkedList<AUserSlide> userSlideList;
+    /**
+     * The layout for the progress bar
+     */
     private Layout progressBarLayout;
 
     /**
@@ -15,39 +22,56 @@ public class ProgressBar {
      */
     public ProgressBar(LinkedList<AUserSlide> _userSlideList){
         this.userSlideList = _userSlideList;
-        this.progressBarLayout = init();
+        init();
     }
 
     /**
      * Builds the progress bar from the user slide elements in the
      * LinkedList and returns it as a finished layout that can then be
      * implemented in the parent layout container.
-     * @return: (Layout) finished progress bar layout
      */
-    public Layout init(){
-         return progressBarLayout;
+    public void init(){
+        VerticalLayout layout = new VerticalLayout();
+
+        this.setProgressBarLayout(layout);
     }
 
     /**
      * Gets the number of user slides in the linked list
-     * @return: (int) number of user slides
+     * @return (int) number of user slides
      */
     private int getNumberSlides(){
         return this.userSlideList.size();
     }
 
+    /**
+     * Returns the UserSlideList
+     * @return (LinkedList) userSlideList
+     */
     public LinkedList<AUserSlide> getUserSlideList() {
-        return userSlideList;
+        return this.userSlideList;
     }
 
+    /**
+     * Set the UserSlideList
+     * @param userSlideList the list with the user slides
+     */
     public void setUserSlideList(LinkedList<AUserSlide> userSlideList) {
         this.userSlideList = userSlideList;
     }
 
+    /**
+     * Returns the current progress bar layout
+     * @return (Layout) progressBarLayout
+     */
     public Layout getProgressBarLayout() {
-        return progressBarLayout;
+        return this.progressBarLayout;
     }
 
+    /**
+     * Sets the progress bar layout
+     * @param progressBarLayout a progress bar layout
+     */
     public void setProgressBarLayout(Layout progressBarLayout) {
         this.progressBarLayout = progressBarLayout;
     }
