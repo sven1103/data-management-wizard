7d45794b0ef39d20f562743524759ab35e31ea6a
heumos <heumos.simon@gmail.com>
2015-05-28 17:19:56 +0200

    in the middle of implementing Document&Content Management

--- src/com/FirstStepsTest.java
+++ src/com/FirstStepsTest.java
@@ -2,13 +2,14 @@ package com;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
+import com.vaadin.data.Item;
 import com.vaadin.event.FieldEvents.TextChangeEvent;
 import com.vaadin.event.FieldEvents.TextChangeListener;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.combobox.FilteringMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
-
 
 @Title("FirstStepsTest")
 @Theme("valo")
@@ -18,78 +19,60 @@ import com.vaadin.ui.VerticalLayout;
  */
 public class FirstStepsTest extends UI {
 
-    protected TextField species;
-    protected TextField projectName;
-    protected TextField personInCharge;
-    protected HorizontalLayout contact;
+    protected Button addDocType;
+    protected ComboBox dataTypes;
+    protected TextArea description;
+
+    protected Table selection;
 
     @Override
     public void init(VaadinRequest request) {
 
 
         // configure Components
-        species = new TextField("Species");
-        species.setValue("Enter Species Name here.");
-        species.setMaxLength(40);
-        species.setColumns(25);
+        selection = new Table("Already Selected.");
 
-        projectName = new TextField("Project Name");
-        projectName.setValue("Dumm Dumm Dumm");
-
-        this.personInCharge = new TextField("Person in Charge");
-        this.personInCharge.setMaxLength(40);
-
-        species.addTextChangeListener(new TextChangeListener() {
+        addDocType = new Button("+");
+        addDocType.addClickListener(new Button.ClickListener() {
             @Override
-            public void textChange(TextChangeEvent textChangeEvent) {
-                System.out.println(textChangeEvent.getText());
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                Notification.show(dataTypes.getValue().toString(),
+                        Notification.Type.TRAY_NOTIFICATION);
+                description.setValue(dataTypes.getValue().toString());
             }
         });
 
-        // Upload
-        TsvUpload tsvUpload = new TsvUpload();
-        tsvUpload.setSlow(true);
+        // Creates a new combobox using an existing container
+        dataTypes = new ComboBox("Select your datatype.");
+        dataTypes.setInputPrompt("No datatype selected.");
+        dataTypes.setInvalidAllowed(false);
+        dataTypes.setNullSelectionAllowed(false);
+        dataTypes.addItem("DOCX");
+        dataTypes.addItem("XLS");
+        dataTypes.addItem("PDF");
+        dataTypes.addItem("TEX");
+        dataTypes.addItem("PNG");
+        // Sets the icon to use with the items
+        // dataTypes.setItemIconPropertyId(ExampleUtil.iso3166_PROPERTY_FLAG);
+        // Set full width
+        dataTypes.setWidth(100.0f, Unit.PERCENTAGE);
 
-        Upload uploader = new Upload(null, tsvUpload);
-        final UploadInfoWindow uploadInfoWindow = new UploadInfoWindow(uploader, tsvUpload);
-        uploader.setImmediate(false);
-        uploader.setButtonCaption("Upload File");
-        uploader.setCaption("Experiment Design Upload from QWizard.");
-        uploader.addStartedListener(new Upload.StartedListener() {
-            @Override
-            public void uploadStarted(final Upload.StartedEvent event) {
-                if (uploadInfoWindow.getParent() == null) {
-                    UI.getCurrent().addWindow(uploadInfoWindow);
-                }
-                uploadInfoWindow.setClosable(false);
-            }
-        });
-        uploader.addFinishedListener(new Upload.FinishedListener() {
-            @Override
-            public void uploadFinished(final Upload.FinishedEvent event) {
-                uploadInfoWindow.setClosable(true);
-            }
-        });
+        // Set the appropriate filtering mode for this example
+        dataTypes.setFilteringMode(FilteringMode.CONTAINS);
+        dataTypes.setImmediate(true);
 
-        // contact layout
-        TextField firstName = new TextField("First name");
-        TextField lastName = new TextField("Last name");
-        TextField phone = new TextField("Phone");
-        TextField email = new TextField("Email");
-        DateField birthDate = new DateField("Birth date");
-        contact = new HorizontalLayout();
-        contact.setCaption("Contact Formular");
-        contact.setMargin(true);
-        contact.setSpacing(true);
-        contact.addComponents(firstName, lastName, phone, email, birthDate);
+        // Disallow null selections
+        dataTypes.setNullSelectionAllowed(false);
+
+        description = new TextArea("Description");
+        description.setValue("Enter description here.");
 
         // buildLayout
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.addComponents(this.addDocType, this.dataTypes, this.description);
         VerticalLayout layout = new VerticalLayout();
-        layout.addComponent(species);
-        layout.addComponent(projectName);
-        layout.addComponent(contact);
-        layout.setExpandRatio(species, 1);
-        layout.addComponent(uploader);
+        layout.addComponent(horizontalLayout);
+        layout.addComponent(this.selection);
         setContent(layout);
 
     }
